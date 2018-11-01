df8617025951de325a5b6eb1218f3a2ff950b3a3
Scott Weinstein <sweinstein27@gmail.com>
2018-11-01 15:01:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// your code here
+const rootURL = 'https://api.github.com';
+
+function getRepositories() {
+  const name = document.getElementById('username').value;
+  const uri = rootURL + '/users/' + name + '/repos';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayRepositories);
+  xhr.open('GET', uri);
+  xhr.send();
+  return false;
+}
